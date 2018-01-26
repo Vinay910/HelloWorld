3de29a1695c37aefa6b777b76ef54b644e420ccc
Vinay910 <vinay910@yahoo.co.in>
2018-01-26 22:05:34 +0530

    Second Commit from IJ

--- src/shortIntByte.java
+++ src/shortIntByte.java
@@ -9,28 +9,64 @@ public class shortIntByte {
         System.out.println(long1);
         unicode();
         operator();
-
-
+        System.out.println("Final Score is : "+score(true,10000,8,200));
+        System.out.println("Final Score is : "+score(true,1000,5,100));
+        System.out.println("Final Score is : "+score(false,10000,8,200));
+        displayHighScorePosition("Vinayak",1500);
+        displayHighScorePosition("Vinay",900);
+        displayHighScorePosition("Vinu",400);
+        displayHighScorePosition("Vin",50);
     }
 
-    public static void floatDouble() {
+    private static void floatDouble() {
         int pound = 99;
         double kg = .45359237 * pound;
         System.out.println("Weight in kg is :- " + kg);
     }
 
-    public static void unicode() {
+    private static void unicode() {
         char ch = '\u00AE';
         System.out.println("Surprise char : " + ch);
     }
 
-    public static void operator()
-    {
-        double d1=20d;
-        double d2=80d;
-        double add=(d1+d2)*25;
-        double rem=add%40;
-        if(rem<=20)
+    private static void operator() {
+        double d1 = 20d;
+        double d2 = 80d;
+        double add = (d1 + d2) * 25;
+        double rem = add % 40;
+        if (rem <= 20)
             System.out.println("Test was over the limit");
+    }
+
+    private static int score(boolean gameOver,int score,int lvl,int bonus){
+        if(gameOver)
+        {
+            return (score+(lvl*bonus));
+        }
+        return 0;
+    }
+
+    private static void displayHighScorePosition(String name,int highScore)
+    {
+        int pos=calculateHighScorePosition(highScore);
+        System.out.println(name+" managed to get into position "+pos);
+    }
+    private static int calculateHighScorePosition(int score)
+    {
+        if(score>1000)
+        {
+            return 1;
+        }
+        else if(score>500 && score<1000)
+        {
+            return 2;
+        }
+        else if(score>100 && score<500)
+        {
+            return 3;
+        }
+        else {
+            return 4;
+        }
     }
 }
